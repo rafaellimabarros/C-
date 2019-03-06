633d7ba1429467c6db0d53ebabb7c6ab78bd8950
Rafael Lima <rafael.redes38@gmail.com>
2019-03-06 13:55:16 -0300

    Adicionado media e c++1

---
+++ media.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+double n1,n2,media;
+
+cout<<"escreva a 1 nota  ";
+cin>>n1;
+cout<<"escreva a 2 nota  ";	
+cin>>n2;
+media=(n1+n2)/2;
+cout<<"a sua media e:  "<<media;
+	
+}
